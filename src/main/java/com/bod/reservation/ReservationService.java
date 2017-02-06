ae205ee0110759ace37b047b9cd76efd5a403c7a
Charlie Draper <charlie.draper@soprasteria.com>
2017-02-06 16:13:16 +0000

    Update ReservationService.java

--- src/main/java/com/bod/reservation/ReservationService.java
+++ src/main/java/com/bod/reservation/ReservationService.java
@@ -1,5 +1,6 @@
 package com.bod.reservation;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,25 +9,46 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ReservationService {
-	
+
 	@Autowired
 	private ReservationRepository rr;
 
-	public List <Reservation> getReservationsByDesk(Long deskId){
-		List <Reservation> reservations = new ArrayList<>();
+	public List<Reservation> getReservationsByDesk(Long deskId) {
+		List<Reservation> reservations = new ArrayList<>();
 		rr.findByDeskId(deskId).forEach(reservations::add);
 		return reservations;
 	}
-	
-	public List <Reservation> getReservations(){
-		List <Reservation> reservations = new ArrayList<>();
+
+	public List<Reservation> getReservations() {
+		List<Reservation> reservations = new ArrayList<>();
 		rr.findAll().forEach(reservations::add);
 		return reservations;
 	}
-	
-	public void addReservation(Reservation reservation){
-		rr.save(reservation);
+
+	public void addReservation(Reservation reservation) {
+		if (reservation.getEnd_time() < reservation.getStart_time()) {
+			System.out.println("Please ensure the start time is after the end time.");
+			return;
+		} else if (compareDates(reservation.getDate()) == false) {
+			System.out.println("Please ensure selected date is not in the past");
+			return;
+		} else if (reservation.getDesk().getAvailability() == false) {
+			System.out.println("Please select an available desk.");
+			return;
+		} else {
+			try{
+			rr.save(reservation);
+			reservation.getDesk().setAvailability(false);
+			} catch(Exception e){
+				
+			}
+		}
 	}
-	
-	
+
+	public boolean compareDates(java.sql.Date a) {
+		LocalDate currentDate = LocalDate.now();
+		LocalDate bookingDate = a.toLocalDate();
+		return currentDate.isBefore(bookingDate);
+	}
+
 }
